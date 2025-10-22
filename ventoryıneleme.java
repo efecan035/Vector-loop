c43af4233d7f01c21caa42f1b4a10d1318ab9c35
efecanacr <efecangg07@gmail.com>
2025-10-22 16:18:19 +0300

    Add files via upload

---
+++ ventoryıneleme.java
@@ -0,0 +1,25 @@
+package proje;
+import java.util.Vector;
+public class ventoryıneleme {
+
+	public static void main(String[] args) {
+		Vector<Integer> numbers = new Vector<>();
+		
+		for(int i= 1; i<= 5; i++) {
+			numbers.add(i*10);
+		}
+		
+		System.out.print("Numbers: ");
+		for(int num : numbers) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+		
+		System.out.print("Numbers using for loop: ");
+		for(int i = 0; i< numbers.size(); i++) {
+			System.out.print(numbers.get(i) + " ");
+		}
+		
+	}
+
+}
